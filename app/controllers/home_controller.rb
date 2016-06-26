f7431c041dab81476c55f531a86a6a7975340c19
Phil Borel <phil_borel@yahoo.com>
2016-06-26 09:57:31 -0400

    adds routes, updates db schema, adds sf  skyline

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  # GET /projects
+  # GET /projects.json
+  def index
+  end
+
+  # GET /projects/1
+  # GET /projects/1.json
+  def show
+  end
+
+  # GET /projects/new
+  def new
+  end
+
+  # GET /projects/1/edit
+  def edit
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160215160119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "github_url"
+    t.string   "url"
+    t.string   "image_url"
+    t.boolean  "finished"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
